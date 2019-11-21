8ca190acae7c174c0e1c4f66d0f8511e64b77a34
Ashik Chowdhury <ashikbracu@gmail.com>
2019-11-22 00:00:05 +0600

    Display all questions

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,16 +3,22 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">All Questions</div>
 
                     <div class="card-body">
                        @foreach($questions as $question)
                             <div class="media">
-
+                                <div class="media-body">
+                                    <h3 class="mt-0">{{$question->title }}</h3>
+                                    {{ Str::limit($question->body, 250) }}
+                                </div>
                             </div>
                        @endforeach
+
+                        {{$questions->links()}}
+
                     </div>
                 </div>
             </div>
