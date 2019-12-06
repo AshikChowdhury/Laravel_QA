2dc6a4c7148a86cbf0c9bce23ebce1fdb2c054be
Ashik Chowdhury <ashikbracu@gmail.com>
2019-12-06 11:51:58 +0600

    update best answer id when answer removed using elequent event

--- app/Answer.php
+++ app/Answer.php
@@ -45,7 +45,12 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 
@@ -61,6 +66,13 @@ class Answer extends Model
      */
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 
 }
