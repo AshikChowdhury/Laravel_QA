065c83fb58cfe9ea9f1585482c7b081b856ef751
Ashik Chowdhury <ashikbracu@gmail.com>
2019-12-08 16:03:22 +0600

    made authorization for choosing best answer

--- app/Http/Controllers/AcceptAnswerController.php
+++ app/Http/Controllers/AcceptAnswerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use App\Answer;
 
@@ -9,6 +10,10 @@ class AcceptAnswerController extends Controller
 {
     public function __invoke(Answer $answer)
     {
+        try {
+            $this->authorize('accept', $answer);
+        } catch (AuthorizationException $e) {
+        }
         $answer->question->acceptBestAnswer($answer);
         return back();
     }
