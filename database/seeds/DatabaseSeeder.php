c717c76286dcc1a60ab5e46a320fe14d2a7d6e4b
Ashik Chowdhury <ashikbracu@gmail.com>
2019-12-10 17:15:54 +0600

    polimorphic relations created for votes and for that created seed file for faker and function created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserQuestionAnswersTableSeeder::class,
             FavoritesTableSeeder::class,
+            VotablesTablesSeeder::class,
         ]);
     }
 }
