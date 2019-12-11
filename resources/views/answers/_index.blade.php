9101f89546ee56b3ec726368e152dc3bfe401d6f
Ashik Chowdhury <ashikbracu@gmail.com>
2019-12-11 17:46:33 +0600

    Convert View in DRY format

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -9,42 +9,7 @@
                 @include('layouts._messages')
                 @foreach($answers as $answer)
                     <div class="media">
-                        <div class="d-fex flex-column vote-controls">
-                            <a title="This answer is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                               onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit()"
-                            >
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-                            <form action="/answers/{{ $answer->id }}/vote" id="up-vote-answer-{{ $answer->id }}"  method="POST" style="display: none">
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                            <span class="vote-counts">{{ $answer->votes_count }}</span>
-                            <a title="This answer is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                               onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit()"
-                            >
-                                <i class="fas fa-caret-down fa-3x"></i>
-                            </a>
-                            <form action="/answers/{{ $answer->id }}/vote" id="down-vote-answer-{{ $answer->id }}"  method="POST" style="display: none">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                            @can('accept', $answer)
-                                <a title="Mark this answer to best answer" class="{{ $answer->status }} mt-2"
-                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit()">
-                                    <i class="fas fa-check fa-2x"></i>
-                                </a>
-                                <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}"  method="POST" style="display: none">
-                                    @csrf
-                                </form>
-                                @else
-                                    @if($answer->is_best)
-                                    <a title="The question owner accepted this answer as best answer" class="{{ $answer->status }} mt-2">
-                                        <i class="fas fa-check fa-2x"></i>
-                                    </a>
-                                    @endif
-                            @endcan
-                        </div>
+                        @include('shared._vote',['model' => $answer ])
                         <div class="media-body">
                             {!! $answer->body_html !!}
                             <div class="row">
@@ -64,15 +29,10 @@
                                 </div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                    <span class="text-muted">Answered {{  $answer->created_date }}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name  }}</a>
-                                        </div>
-                                    </div>
+                                    @include('shared._author', [
+                                        'model' => $answer,
+                                        'label' => 'answered'
+                                        ])
                                 </div>
                             </div>
                         </div>
---
+++ resources/views/shared/_vote.blade.php
@@ -0,0 +1,42 @@
+@if($model instanceof App\Question)
+    @php
+        $name = 'question';
+        $firestURISegment = 'questions';
+    @endphp
+@elseif($model instanceof App\Answer)
+    @php
+        $name = 'answer';
+        $firestURISegment = 'answers';
+    @endphp
+@endif
+
+{{--@php--}}
+{{--    $fromId = $name . "-" . $model->id;--}}
+{{--@endphp--}}
+
+<div class="d-fex flex-column vote-controls">
+    <a title="This {{$name}} is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
+       onclick="event.preventDefault(); document.getElementById('up-vote-{{$name}}-{{ $model->id }}').submit()"
+    >
+        <i class="fas fa-caret-up fa-3x"></i>
+    </a>
+    <form action="/{{ $firestURISegment }}/{{ $model->id }}/vote" id="up-vote-{{ $fromId }}"  method="POST" style="display: none">
+        @csrf
+        <input type="hidden" name="vote" value="1">
+    </form>
+    <span class="vote-counts">{{ $model->votes_count }}</span>
+    <a title="This {{$name}} is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
+       onclick="event.preventDefault(); document.getElementById('down-vote-{{ $fromId }}').submit()"
+    >
+        <i class="fas fa-caret-down fa-3x"></i>
+    </a>
+    <form action="/{{ $firestURISegment }}/{{ $model->id }}/vote" id="down-vote-{{ $fromId }}"  method="POST" style="display: none">
+        @csrf
+        <input type="hidden" name="vote" value="-1">
+    </form>
+    @if($model instanceof App\Question)
+        @include('shared._favorite', ['model' => $model])
+    @elseif($model instanceof App\Answer)
+        @include('shared._accept', ['model' => $model])
+    @endif
+</div>
