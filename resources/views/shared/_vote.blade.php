8592da527692676cea9b43c10d812b79c59aa43c
Ashik Chowdhury <ashikbracu@gmail.com>
2020-01-01 17:22:45 +0600

    tidy up the views

--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -12,21 +12,21 @@
 
 <div class="d-fex flex-column vote-controls">
     <a title="This {{$name}} is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-       onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formId }}').submit()"
+       onclick="event.preventDefault(); document.getElementById('up-vote-{{ $name }}-{{ $model->id }}').submit()"
     >
         <i class="fas fa-caret-up fa-3x"></i>
     </a>
-    <form action="/{{ $firestURISegment }}/{{ $model->id }}/vote" id="up-vote-{{ $fromId }}"  method="POST" style="display: none">
+    <form action="/{{ $firestURISegment }}/{{ $model->id }}/vote" id="up-vote-{{ $name }}-{{ $model->id }}"  method="POST" style="display: none">
         @csrf
         <input type="hidden" name="vote" value="1">
     </form>
     <span class="vote-counts">{{ $model->votes_count }}</span>
     <a title="This {{$name}} is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-       onclick="event.preventDefault(); document.getElementById('down-vote-{{ $fromId }}').submit()"
+       onclick="event.preventDefault(); document.getElementById('down-vote-{{ $name }}-{{ $model->id }}').submit()"
     >
         <i class="fas fa-caret-down fa-3x"></i>
     </a>
-    <form action="/{{ $firestURISegment }}/{{ $model->id }}/vote" id="down-vote-{{ $fromId }}"  method="POST" style="display: none">
+    <form action="/{{ $firestURISegment }}/{{ $model->id }}/vote" id="down-vote-{{ $name }}-{{ $model->id }}"  method="POST" style="display: none">
         @csrf
         <input type="hidden" name="vote" value="-1">
     </form>
